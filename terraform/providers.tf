7661e3a64bd578035c90903a380269ca7ceb9950
pradeepchoudhary31 <pradeepchoudhary.88847@gmail.com>
2025-06-21 16:56:21 +0530

    added backend s3

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "mini-commerce-tf-state"     
+    key            = "terraform/state.tfstate"    # Path inside the bucket
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"            # Optional but recommended
+  }
 }
 
 provider "aws" {
